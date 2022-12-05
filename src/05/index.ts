26855030ca5c3fd4ab600beb81cc83307e7e6455
Andrei Sirotin <andrei.sirotin@pipedrive.com>
2022-12-05 08:49:00 +0200

    day 5 cleanup

--- src/05/index.ts
+++ src/05/index.ts
@@ -1,62 +1,58 @@
-export const main = (input: string): unknown => {
+const getStackAndInstructions = (input: string): [string[][], string] => {
   const [crates, instructions] = input.split('\n\n');
   const stackArray = crates
     .split('\n')
     .map((row) => row.match(/.{3,4}/g)?.map((s) => s.trim()));
   const columns = stackArray.pop();
-  const stacks = columns?.map((column, i) =>
-    stackArray
-      .map((row) => row?.[i])
-      .reverse()
-      .filter(Boolean),
-  );
+  const stacks =
+    columns
+      ?.map((column, i) =>
+        stackArray
+          .map((row) => row?.[i])
+          .reverse()
+          .filter(Boolean),
+      )
+      .map((s) => s.filter((ss): ss is string => ss !== undefined)) ?? [];
+  return [stacks, instructions];
+};
+
+const getInstruction = (s: string): [number, number, number] => {
+  const [, a, b, c] = [...s.matchAll(/move (\d+) from (\d+) to (\d+)/g)][0];
+  const move = Number(a);
+  const from = Number(b) - 1;
+  const to = Number(c) - 1;
+  return [move, from, to];
+};
+const getResult = (stacks: string[][]): string =>
+  stacks
+    .map((s) => s.pop())
+    .join()
+    .replaceAll(/[[\],]/g, '');
+
+export const main = (input: string): unknown => {
+  const [stacks, instructions] = getStackAndInstructions(input);
 
   instructions?.split('\n').forEach((instruction) => {
-    const [, a, b, c] = [
-      ...instruction.matchAll(/move (\d+) from (\d+) to (\d+)/g),
-    ][0];
-    const move = Number(a);
-    const from = Number(b) - 1;
-    const to = Number(c) - 1;
+    const [move, from, to] = getInstruction(instruction);
     for (let i = 0; i < move; i++) {
-      stacks?.[to].push(stacks?.[from].pop());
+      stacks[to].push(stacks[from].pop() || '');
     }
   });
-  return stacks
-    ?.map((s) => s.pop())
-    .join()
-    .replaceAll(/\[|\]|,/g, '');
+  return getResult(stacks);
 };
 
 export const main2 = (input: string): unknown => {
-  const [crates, instructions] = input.split('\n\n');
-  const stackArray = crates
-    .split('\n')
-    .map((row) => row.match(/.{3,4}/g)?.map((s) => s.trim()));
-  const columns = stackArray.pop();
-  const stacks = columns?.map((column, i) =>
-    stackArray
-      .map((row) => row?.[i])
-      .reverse()
-      .filter(Boolean),
-  );
+  const [stacks, instructions] = getStackAndInstructions(input);
 
-  instructions?.split('\n').forEach((instruction) => {
-    const [, a, b, c] = [
-      ...instruction.matchAll(/move (\d+) from (\d+) to (\d+)/g),
-    ][0];
-    const move = Number(a);
-    const from = Number(b) - 1;
-    const to = Number(c) - 1;
+  instructions.split('\n').forEach((instruction) => {
+    const [move, from, to] = getInstruction(instruction);
+
     const stack = [];
     for (let i = 0; i < move; i++) {
-      stack.push(stacks?.[from].pop());
+      stack.push(stacks[from].pop() || '');
     }
     stack.reverse();
-    stacks?.[to].push(...stack);
+    stacks[to].push(...stack);
   });
-  return stacks
-    ?.map((s) => s.pop())
-    .join()
-    .replaceAll(/\[|\]|,/g, '');
+  return getResult(stacks);
 };
