c7cee39129f2183767b3a4e0236ef7c9d69b5e87
Andrei Sirotin <andrei.sirotin@pipedrive.com>
2024-12-03 08:40:38 +0200

    update config

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,4 +2,14 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
 };
