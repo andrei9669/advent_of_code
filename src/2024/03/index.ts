4fe843ccdec647b3645785bd779b4f9249766b50
Andrei Sirotin <andrei.sirotin@pipedrive.com>
2024-12-03 08:29:50 +0200

    day 3 part 2 test works

--- src/2024/03/index.ts
+++ src/2024/03/index.ts
@@ -16,6 +16,48 @@ export function main(input: string): number {
   return sum;
 }
 
-export function main2(_input: string): number {
-  return 0;
+function containsRegexAtIndex(
+  str: string,
+  pattern: RegExp,
+  index: number,
+): boolean {
+  const substring = str.substring(index);
+  const res = pattern.test(substring);
+  pattern.lastIndex = 0;
+  return res;
+}
+
+export function main2(input: string): number {
+  const mulRegex = /^mul\((?<left>\d+),(?<right>\d+)\)/g;
+  const doRegex = /^do\(\)/g;
+  const doNotRegex = /^don't\(\)/g;
+
+  let sum = 0;
+  let doMul = true;
+  for (let i = 0; i < input.length; i++) {
+    mulRegex.lastIndex = 0;
+    if (doMul) {
+      let res = mulRegex.exec(input.substring(i));
+      if (res?.groups) {
+        const {
+          groups: { left, right },
+        } = res;
+        sum += Number(left) * Number(right);
+        i += res[0].length;
+      }
+      const doNotMatch = input.substring(i).match(doNotRegex);
+      if (doNotMatch) {
+        doMul = false;
+        i += doNotMatch[0].length;
+      }
+    } else {
+      const doMatch = input.substring(i).match(doRegex);
+      if (doMatch) {
+        doMul = true;
+        i += doMatch[0].length;
+      }
+    }
+  }
+
+  return sum;
 }
